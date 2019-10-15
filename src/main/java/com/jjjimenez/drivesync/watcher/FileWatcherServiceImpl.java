778b3d06f5d7f2310609243a5ecebfe888fd4ba1
Joshua <jimenez.johnjoshua.jjj@gmail.com>
2019-10-15 21:48:40 +0800

    Implement FileWatcherService :sparkles:

--- src/main/java/com/jjjimenez/drivesync/watcher/FileWatcherServiceImpl.java
+++ src/main/java/com/jjjimenez/drivesync/watcher/FileWatcherServiceImpl.java
@@ -1,42 +1,119 @@
 package com.jjjimenez.drivesync.watcher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static java.nio.file.StandardWatchEventKinds.*;
 
-final class FileWatcherServiceImpl {
-    private final Map<Path, WatchKey> watchedPaths;
+final class FileWatcherServiceImpl implements FileWatcherService {
+    private static final Logger logger = LoggerFactory.getLogger(FileWatcherServiceImpl.class);
+
+    private final Map<Path, WatchKey> watchedFiles;
     private final WatchService fileSystemWatchService;
 
-    FileWatcherServiceImpl() throws IOException {
-        watchedPaths = new HashMap<>();
-        fileSystemWatchService = FileSystems.getDefault().newWatchService();
+    private final CreateWatchStateListener createWatchStateListener;
+    private final DeleteWatchStateListener deleteWatchStateListener;
+    private final ModifyWatchStateListener modifyWatchStateListener;
+    private final OverflowWatchStateListener overflowWatchStateListener;
+
+    private FileWatcherServiceImpl(Builder builder) {
+        this.watchedFiles = builder.watchedFiles;
+        this.fileSystemWatchService = builder.fileSystemWatchService;
+        this.createWatchStateListener = builder.createWatchStateListener;
+        this.deleteWatchStateListener = builder.deleteWatchStateListener;
+        this.modifyWatchStateListener = builder.modifyWatchStateListener;
+        this.overflowWatchStateListener = builder.overflowWatchStateListener;
     }
 
-    WatchKey registerPath(Path path, WatchEvent.Kind... eventKinds) throws IOException {
+    static class Builder {
+        private Map<Path, WatchKey> watchedFiles;
+        private WatchService fileSystemWatchService;
+
+        private CreateWatchStateListener createWatchStateListener;
+        private DeleteWatchStateListener deleteWatchStateListener;
+        private ModifyWatchStateListener modifyWatchStateListener;
+        private OverflowWatchStateListener overflowWatchStateListener;
+
+        public Builder() throws IOException {
+            this.watchedFiles = new HashMap<>();
+            this.fileSystemWatchService = FileSystems.getDefault().newWatchService();
+        }
+
+        public Builder createWatchStateListener(CreateWatchStateListener createWatchStateListener) {
+            this.createWatchStateListener = createWatchStateListener;
+            return this;
+        }
+
+        public Builder deleteWatchStateListener(DeleteWatchStateListener deleteWatchStateListener) {
+            this.deleteWatchStateListener = deleteWatchStateListener;
+            return this;
+        }
+
+        public Builder modifyWatchStateListener(ModifyWatchStateListener modifyWatchStateListener) {
+            this.modifyWatchStateListener = modifyWatchStateListener;
+            return this;
+        }
+
+        public Builder overflowWatchStateListener(OverflowWatchStateListener overflowWatchStateListener) {
+            this.overflowWatchStateListener = overflowWatchStateListener;
+            return this;
+        }
+
+        public Builder watchedFiles(Map<Path, WatchKey> watchedFiles) {
+            this.watchedFiles = watchedFiles;
+            return this;
+        }
+
+        public Builder fileSystemWatchService(WatchService fileSystemWatchService) {
+            this.fileSystemWatchService = fileSystemWatchService;
+            return this;
+        }
+
+        public FileWatcherServiceImpl build() {
+            return new FileWatcherServiceImpl(this);
+        }
+    }
+
+    @Override
+    public Map<Path, WatchKey> getWatchedFiles() {
+        return Collections.unmodifiableMap(watchedFiles);
+    }
+
+    @Override
+    public WatchKey watchFile(Path path, WatchEvent.Kind... eventKinds) throws IOException {
         WatchKey key = path.register(fileSystemWatchService, eventKinds);
-        watchedPaths.put(path, key);
+        watchedFiles.put(path, key);
 
         return key;
     }
 
-    void registerAllPaths(List<Path> paths) throws IOException {
-        for(Path path: paths) {
-            registerPath(path, OVERFLOW, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+    @Override
+    public void startFileWatcherService() {
+        WatchKey watchedFile;
+
+        logger.info("Polling for file state changes");
+        while((watchedFile = fileSystemWatchService.poll()) != null) {
+            for(WatchEvent<?> event: watchedFile.pollEvents()) {
+                WatchEvent.Kind<?> kind = event.kind();
+                if(kind == OVERFLOW && overflowWatchStateListener != null) {
+                    overflowWatchStateListener.overflowStateChange();
+                } else if(kind == ENTRY_CREATE && createWatchStateListener != null) {
+                    createWatchStateListener.createStateChange();
+                } else if(kind == ENTRY_DELETE && deleteWatchStateListener != null) {
+                    deleteWatchStateListener.deleteStateChange();
+                } else if(kind == ENTRY_MODIFY && modifyWatchStateListener != null) {
+                    modifyWatchStateListener.modifyStateChange();
+                } else {
+                    logger.info("No provided state listeners.");
+                }
+            }
+            watchedFile.reset();
         }
-    }
-
-    void startDirectoryWatcher() { }
-
-    public Map<Path, WatchKey> getWatchedPaths() {
-        return watchedPaths;
-    }
-
-    public WatchService getFileSystemWatchService() {
-        return fileSystemWatchService;
     }
 }
